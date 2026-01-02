8dd88ef90b23bae27aac84b2a10b44faaf34a5c9
Eden Cohen <eededeedencohen@gmail.com>
2026-01-02 15:56:15 +0200

    deploy

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import { createRoot } from "react-dom/client";
 import { StrictMode } from "react";
 import { BrowserRouter } from "react-router-dom";
-import { ProductContextProvider2 } from "./context/ProductContext2";
 import { GymMemberProvider } from "./context/GymMemberContext.jsx"; // ייבוא הפרוביידר החדש
 
 
@@ -12,9 +11,7 @@ createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
       <GymMemberProvider>
-        <ProductContextProvider2>
           <App />
-        </ProductContextProvider2>
       </GymMemberProvider>
     </BrowserRouter>
   </StrictMode>
